58fea26410a86a27ae56809690627aed078fb18b
Alex Lopez <alope6@uic.edu>
2019-11-28 00:06:30 -0600

    added stuff

---
+++ Assets/Game1Gen.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game1Gen : MonoBehaviour
+{
+    public GameObject enemy;
+    public Vector3 spawnLocation;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CreateCube()
+    {
+        spawnLocation = new Vector3(0, Random.value * 10, 0);
+        Instantiate(enemy, spawnLocation, Quaternion.identity);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("collision detected");
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
+        if (collision.relativeVelocity.magnitude > 2)
+            CreateCube();
+    }
+}
